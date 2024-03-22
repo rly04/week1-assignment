da82c53a03c11b7027db8d365a205522c93d3d24
riley <rearle1706@gmail.com>
2024-03-22 15:22:47 +0000

    week1-assignment

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log("Haaii");
+// console.log("Haaii");
+const music = document.getElementById("better");
+const playBtn = document.getElementById("playBtn");
+const pauseBtn = document.getElementById("pauseBtn");
+const stopBtn = document.getElementById("stopBtn");
+const skipBack = document.getElementById("skipBack");
+const skipFor = document.getElementById("skipFor");
+
+playBtn.addEventListener("click", function () {
+  music.play();
+});
+
+pauseBtn.addEventListener("click", function () {
+  music.pause();
+});
+
+stopBtn.addEventListener("click", function () {
+  music.pause();
+  music.currentTime = 0;
+});
+
+skipBack.addEventListener("click", function () {
+  music.currentTime = music.currentTime - 10;
+});
+
+skipFor.addEventListener("click", function () {
+  music.currentTime = music.currentTime + 10;
+});
